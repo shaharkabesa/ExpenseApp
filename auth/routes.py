d7afab6aa2ac3e3e1006bb2b6d1cde5616458517
Shahar-Kabesa <61517666+shaharkabesa@users.noreply.github.com>
2025-02-18 23:08:40 +0200

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect,sessions,session
 
 auth = Blueprint('auth', __name__, template_folder='templates')
+auth.secret_key  = "'SSD!@#L%!E'"
 
-@auth.route('/login')
+@auth.route('/login', methods=['POST','GET'])
 def login():
+    if 'username' in session:
+        return render_template('login.html', name=session['username'])
+    if request.method == "POST":
+        data = request.form
+        username = data['username']
+        session['username'] = username
+        password = data['password']
+        print(data['username'])
+        return render_template('login.html', name=username)
     return render_template('login.html')
